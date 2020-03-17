d8bd58d8ad840d62bf28fabcb163046c6b11267e
Lorena Gomes <42523044+Lorenagoc@users.noreply.github.com>
2020-03-17 17:55:49 -0300

    Delete _primary.vhd

--- banco de registradores/work/decodificador/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity decodificador is
-    port(
-        entrada         : in     vl_logic_vector(2 downto 0);
-        saida           : out    vl_logic_vector(7 downto 0)
-    );
-end decodificador;
